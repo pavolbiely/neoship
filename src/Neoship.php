fe3817440f45a9f39f84387fb7a7230081a90cb2
Pavol Biely <biely@webtec.sk>
2018-01-21 04:54:46 +0100

    Fixed CURL options by setting CURLOPT_FOLLOWLOCATION to true in sendHttpRequest() method.

--- src/Neoship.php
+++ src/Neoship.php
@@ -311,6 +311,7 @@ class Neoship
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		if (($response = curl_exec($ch)) === false) {
